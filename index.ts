601d58d62f82b3416624c1abe0fe29d5043b2978
maelstonly <92147460+maelstonly@users.noreply.github.com>
2021-11-05 08:18:45 +0100

    6 hours

--- index.ts
+++ index.ts
@@ -44,7 +44,7 @@ interface TransformationsMap {
 }
 const EVENTS_PER_BATCH = 2000
 const RUN_LIMIT = 20
-const WHEN_DONE_NEXT_JOB_SCHEDULE_SECONDS = 1800
+const WHEN_DONE_NEXT_JOB_SCHEDULE_SECONDS = 21600
 const IS_CURRENTLY_IMPORTING = 'new_key_2'
 const TRANSFORMATION_NAME = 'default'
 const sanitizeSqlIdentifier = (unquotedIdentifier: string): string => {
