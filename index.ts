e711e1283261f19087cde985ccefec544947aec2
maelstonly <92147460+maelstonly@users.noreply.github.com>
2021-12-22 16:09:35 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -45,7 +45,7 @@ interface TransformationsMap {
 const EVENTS_PER_BATCH = 200
 const RUN_LIMIT = 20
 const WHEN_DONE_NEXT_JOB_SCHEDULE_SECONDS = 2
-const IS_CURRENTLY_IMPORTING = 'new_key_20'
+const IS_CURRENTLY_IMPORTING = 'new_key_21'
 const TRANSFORMATION_NAME = 'users_group'
 const sanitizeSqlIdentifier = (unquotedIdentifier: string): string => {
     return unquotedIdentifier
@@ -90,7 +90,7 @@ export const setupPlugin: RedshiftImportPlugin['setupPlugin'] = async ({ config,
     
     console.log('launching job')
     const totalRowsToImport = await getTotalRowsToImport(config)
-    /*await jobs.importAndIngestEvent({ retriesPerformedSoFar: 0, successiveRuns: 0 }).runIn(10, 'seconds')*/
+    const jobs.importAndIngestEvent({ retriesPerformedSoFar: 0, successiveRuns: 0 }).runIn(10, 'seconds')
     console.log('job finished')
 }
 
